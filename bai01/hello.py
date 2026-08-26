bbc2df40526a418ef2e36872c2b12aae59f47a60
Phan Quang Huy <phanquanghuy22006@gmail.com>
2026-08-26 09:43:17 +0700

    Update hello.py

--- bai01/hello.py
+++ bai01/hello.py
@@ -1,29 +1,3 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "a5beecb3",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "ten = \"Phan Quang Huy\"\n",
-    "print(\"Xin chao,\", ten)\n",
-    "print(\"Day la chuong trinh Python dau tien cua toi.\")"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "name": "python",
-   "version": "3.13.15"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+ten = "Phan Quang Huy"
+print("Xin chao,", ten)
+print("Day la chuong trinh Python dau tien cua toi.")
